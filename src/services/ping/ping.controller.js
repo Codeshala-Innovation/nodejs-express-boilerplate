56c77b4d3d8914b14f5d6ad2fc195778adea033c
Tolsee <tolsee3@gmail.com>
2018-10-21 17:54:27 +0545

    Ping route

--- src/services/ping/ping.controller.js
+++ src/services/ping/ping.controller.js
@@ -1,1 +1,8 @@
 // @flow
+
+export const Get = (req: Object, res: Object) => {
+  return res.status(200).json({
+    status: 'success'
+  });
+};
+
--- src/services/ping/ping.routes.js
+++ src/services/ping/ping.routes.js
@@ -1,1 +1,7 @@
 // @flow
+
+import { Get } from "./ping.controller";
+
+export default app => {
+  app.route('/ping').get(Get);
+}
