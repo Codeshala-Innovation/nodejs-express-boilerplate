33edc90158e907329e08e3bbefc3ee30593d4a3a
Tolsee <tolsee3@gmail.com>
2018-10-21 18:16:39 +0545

    Autoload module

--- src/utils/autoLoadRoutes.js
+++ src/utils/autoLoadRoutes.js
@@ -1,3 +1,14 @@
 // @flow
+import * as fs from 'fs';
+import path from 'path';
 
-// Load all the routes automatically
+export default app => {
+  fs.readdir(path.resolve(__dirname, '../services'), function (err, folders) {
+    folders.forEach(folder => {
+      fs.readdir(path.resolve(__dirname, '../services', folder), function (err, files) {
+        const requiredFiles = files.filter(file => file.match(/.routes.js$/));
+        requiredFiles.forEach(file => require(path.resolve(__dirname, '../services', folder, file))(app));
+      })
+    });
+  });
+};
