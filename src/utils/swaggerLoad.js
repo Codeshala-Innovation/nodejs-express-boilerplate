95394485c80859b6dc87110a1877cf39f084d78f
Tolsee <tolsee3@gmail.com>
2018-10-31 01:37:36 +0545

    Swagger documentation

---
+++ src/utils/swaggerLoad.js
@@ -0,0 +1,26 @@
+import YAML from 'js-yaml';
+import fs from 'fs';
+import { resolveRefs as resolve } from "json-refs";
+import swaggerUi from "swagger-ui-express";
+
+const parser = (directory) => {
+  // Documentation
+  const root = YAML.load(fs.readFileSync(directory).toString());
+  const options = {
+    filter        : ['relative', 'remote'],
+    loaderOptions : {
+      processContent : function (res, callback) {
+        callback(null, YAML.safeLoad(res.text));
+      }
+    }
+  };
+
+  return resolve(root, options);
+};
+
+const swaggerLoad = (app, directory) => {
+  parser(directory)
+    .then(document => app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(document.resolved)));
+};
+
+export default swaggerLoad;
